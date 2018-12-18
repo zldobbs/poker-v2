880400da39683fb8144d02e597db131eb6e2135b
Zach Dobbs <dobbszach@gmail.com>
2018-12-17 18:26:55 -0600

    Adjust button styles

--- client/src/components/ActionBar.js
+++ client/src/components/ActionBar.js
@@ -18,16 +18,16 @@ import 'materialize-css/dist/css/materialize.min.css';
 class ActionBar extends Component {
     render() {
         return(
-            <div className="col s6 m9">
-                <div className="row">
+            <div className="col s6 m9 push-m2">
+                <div className="row center">
                     <div className="col s12 m3">
-                        <button className="btn waves-effect action-btn">Fold</button>
+                        <button className="btn waves-effect action-btn redish">Fold</button>
                     </div>
                     <div className="col s12 m3">
-                        <button className="btn waves-effect action-btn">Check</button>
+                        <button className="btn waves-effect action-btn blueish-lt black-text">Check</button>
                     </div>
                     <div className="col s12 m3">
-                        <button className="btn waves-effect action-btn">Bet</button>
+                        <button className="btn waves-effect action-btn blueish-dk">Bet</button>
                     </div>
                 </div>
             </div>
